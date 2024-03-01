4c764b710b1bd5d554945f9df25cc099d43f0378
thesrtagomes <pamela.gomes.work@gmail.com>
2024-03-01 00:41:14 -0300

    first commit

---
+++ day_2.py
@@ -0,0 +1,11 @@
+print("100 days of coding: day 2")
+name = input("What is your name? ")
+favorite_food = input("What is your favorite food? ")
+favorite_music = input("What is your favorite music? ")
+location = input("Where do you live? ")
+print()
+print(
+    f"""
+You are {name}. You are probably hungry for {favorite_food} and you're definitely getting your groove on to {favorite_music}.  Living in the amazing {location}.
+"""
+)
