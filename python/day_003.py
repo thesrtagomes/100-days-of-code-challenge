85ed59293f3e8efd0fab76ed07e0cc36b62569af
thesrtagomes <pamela.gomes.work@gmail.com>
2024-03-02 01:33:59 -0300

    day 3 of challenge

---
+++ python/day_003.py
@@ -0,0 +1,8 @@
+food = input("What is your favorite food? ")
+plant = input("What is your favorite plant? ")
+cooking = input("What is your favorite method of cooking? ")
+burned = input("What is a word to describe burned food? ")
+item = input("What is your favorite household item? ")
+print()
+print("MENU")
+print(cooking, food, "with", burned, plant, "on a bed of", item)
