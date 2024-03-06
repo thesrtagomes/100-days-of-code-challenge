fa8dcf7eef73377d329fcd366e5279e8bf8498b8
thesrtagomes <pamela.gomes.work@gmail.com>
2024-03-05 23:59:27 -0300

    day 7

---
+++ python/day_007.py
@@ -0,0 +1,17 @@
+# 100 days of code: day 7
+tv_show = input("What is your favorite TV show?")
+if tv_show == "My Demon":
+    print("I love My Demon!")
+    fav_character = input("What is your favorite character?")
+    if fav_character == "Do Do-hee":
+        print("I love her character too!")
+    else:
+        print(f"Hmm, {fav_character} seems to be interesting")
+
+elif tv_show == "Tomorrow":
+    print("I am watching Tomorrow with my boyfriend and I love it!")
+    fav_character = input("What is your favorite character?")
+    if fav_character == "Goo Ryeon":
+        print("Yes! She is perfect! She is such a great character!")
+    elif fav_character == "Park Joong Gil":
+        print("Oh no. He is too ")
