8027e243bb9b345f8ad7ddf4c411e0e8484f08b6
thesrtagomes <pamela.gomes.work@gmail.com>
2024-03-06 23:57:32 -0300

    day 8

---
+++ python/day_008.py
@@ -0,0 +1,7 @@
+# 100 days of code: day 8
+print('Welcome to affirmation generator!')
+print()
+name = input('What is your name? ')
+day_of_the_week =  input('What day of the week is today? ')
+color = input('What is your favorite color? ')
+food = input('What is your favorite food? ')
