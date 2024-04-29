466e1827625e182dd554464d1cc98e4e61d87fb0
thesrtagomes <pamela.gomes.work@gmail.com>
2024-04-28 21:35:06 -0300

    day 18 and 19

---
+++ python/day_018.py
@@ -0,0 +1,19 @@
+number = 874649
+attempts = 0
+
+while True:
+    user_guess = int(input("Guess a number between 1 and 1,000,000: "))
+    if user_guess < number and user_guess > 0:
+        print('Too low. Try again.')
+        attempts += 1
+    elif user_guess > number:
+        print('Too high. Try again.')
+        attempts += 1
+    elif user_guess < 0:
+        exit()
+    else:
+        attempts += 1
+        break
+print(f'''Congratulations😊⭐🚀 
+The number {number} is the correct number. 
+It took you {attempts} attempt(s). Awesome!🚀🥇''')
