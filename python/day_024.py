0465c3cd1aec939089d85cdb794a76371a33d114
thesrtagomes <pamela.gomes.work@gmail.com>
2024-05-24 13:29:46 -0300

    day 24

---
+++ python/day_024.py
@@ -0,0 +1,25 @@
+#day 24/100 - Pamela Gomes
+#Parameters
+import random
+
+print('The Infinity Dice Game 🎲🎮')
+print()
+
+def roll_dice(user_sides):
+    random_sides = random.randint(1, user_sides)
+    print('You rolled ',random_sides)
+
+user_sides = int(input('How many sides? '))
+
+while True:
+    roll_dice(user_sides)
+    play_again = input('Roll again? ').lower()
+    if play_again != "yes":
+     break
+
+  
+  
+  
+
+  
+ 
